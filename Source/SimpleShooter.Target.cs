c805fdc2bbace158236f89efe19c03c260f36b7c
Marc Lynch <marcwlynch@gmail.com>
2021-02-27 16:20:50 -0800

    Fleshed out actors/level
    * Added character and pawn
    * Added default map

---
+++ Source/SimpleShooter.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SimpleShooterTarget : TargetRules
+{
+	public SimpleShooterTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "SimpleShooter" } );
+	}
+}
