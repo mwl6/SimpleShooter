ee8a7bae8fc9e7796789174e276c8a1bc8e078d7
Marc Lynch <marcwlynch@gmail.com>
2021-02-27 20:32:27 -0800

    Added movement
    * Provided input commands for basic movement (look, move, jump)

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -19,11 +19,15 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
 
 };
