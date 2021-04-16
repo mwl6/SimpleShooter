f1d983691c8c84e8b05921ba7fa23d2a4d572f83
Marc Lynch <marcwlynch@gmail.com>
2021-04-15 22:19:07 -0700

    Clean up gameplay interaction

---
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterPlayerController.h"
+
+
+void AShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner)
+{
+    Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+    UE_LOG(LogTemp, Warning, TEXT("We have finished!"));
+}
